5ced32a611f2d04b85f23804bf7c717ae9954383
OneBeatTrue <57670498+OneBeatTrue@users.noreply.github.com>
2020-12-01 19:59:39 +0400

    Update n1.py

--- n1.py
+++ n1.py
@@ -6,53 +6,25 @@ import time
 import os
 
 
+# TOKEN = TOKEN
+
+
 class YLBotClient(discord.Client):
     async def on_ready(self):
-        print(f'{self.user} has connected to Discord!')
+        print(f'{self.user} присоединился к Discord!')
         for guild in self.guilds:
             print(
-                f'{self.user} has connected to chat:\n'
-                f'{guild.name}(id: {guild.id})'
-                f'I am ready to to track students.')
+                f'{self.user} присоединился к чату:\n'
+                f'{guild.name}(id: {guild.id})\n'
+                f'Я готов следить за посещаемостью.')
         category = self.guilds[0].categories[0]
         channel = category.channels[0]
-        # await channel.send('Hello, my name is PopTracker.')
-        # await channel.send('I am ready to to track students.')
-        # await channel.send('To get the instructions, type !help')
-        return
+        await channel.send('Здравствуйте, я бот PopTracker.')
+        await channel.send('Отслеживаю посещаемость.')
+        await channel.send('Чтобы получить инструкцию, введите !help')
 
     async def on_group_join(self, channel, user):
         print(channel, user)
-        return
-
-    def transliteration(self, st):
-        a = {'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'E',
-             'Ж': 'Zh', 'З': 'Z', 'И': 'I', 'Й': 'I', 'К': 'K', 'Л': 'L', 'М': 'M',
-             'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U',
-             'Ф': 'F', 'Х': 'Kh', 'Ц': 'Tc', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch',
-             'Ы': 'Y', 'Э': 'E', 'Ю': 'Iu', 'Я': 'Ia', 'а': 'a', 'б': 'b', 'в': 'v',
-             'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'e', 'ж': 'zh', 'з': 'z', 'и': 'i',
-             'й': 'i', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p',
-             'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'tc',
-             'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ы': 'y', 'э': 'e', 'ю': 'iu', 'я': 'ia'}
-        y = []
-        w = ''
-        s = st
-        s = s.split()
-        for i in range(len(s)):
-            t = s[i]
-            for u in range(len(t)):
-                if t[u] in a:
-                    w += a[t[u]]
-                else:
-                    if t[u] == 'ъ' or t[u] == 'ь' or t[u] == 'Ъ' or t[u] == 'Ь':
-                        continue
-                    else:
-                        w += t[u]
-            if len(w) != 0:
-                y.append(w)
-                w = ''
-        return ' '.join(y)
 
     def is_admin(self, message: discord.Message) -> bool:
         chann = message.channel
@@ -106,77 +78,86 @@ class YLBotClient(discord.Client):
         if not self.is_admin(message):
             return
         if "!help" in message.content.lower():
-            await message.channel.send('I am a bot that tracks class attendance.')
-            await message.channel.send('To make a tracking lesson you need to print !set_lesson H M L E '
-                                       '(H and M - time when the lesson should start: hours and minutes; L-duration; '
-                                       'E - email address to send the report to)')
-            await message.channel.send('To get the instructions again, type !help again.')
+            await message.channel.send('Я бот для остлеживания присутствующих на уроке.')
+            await message.channel.send('Чтобы отследить посещаемость урока, введите (на латинском) !set_lesson H M L '
+                                       '(H и M - начало урока: часы и минуты; L - длительность)')
+            await message.channel.send('Чтобы получить инструкцию снова, введите !help')
         if "!set_lesson" in message.content.lower():
             a = message.content.lower().split()
             lesson = {}
             dop = []
-            if not len(a) == 5:
-                await message.channel.send('Invalid number of arguments!')
+            if not len(a) == 4:
+                await message.channel.send('Неверное число аргументов!')
                 return
             a = a[1:]
             for i in a[:3]:
                 if i.isdigit():
                     dop.append(int(i))
                 else:
-                    await message.channel.send('Invalid argument values!')
+                    await message.channel.send('Введите корректные значения!')
                     return
-            dop.append(str(a[3]))
-            if not self.is_correct_email(dop[3]):
-                await message.channel.send('You are trying to set a wrong email!')
-                return
             if not (0 <= dop[0] <= 23 and 0 <= dop[1] <= 60):
-                await message.channel.send('You are trying to set a wrong time!')
+                await message.channel.send('Установите корректное время!')
                 return
-            print(f'{message.author} set the lesson.')
-            s = ['' if i == 1 else 's' for i in dop[:3]]
-            await message.channel.send(f'The lesson should start at {dop[0]} hour{s[0]} {dop[1]} '
-                                       f'minute{s[1]} and lasts {dop[2]} minute{s[2]}.')
+            print(f'Пользователь {message.author} запланировал урок.')
+            s = []
+            for i in dop[:3]:
+                if i % 10 == 1 and i % 100 != 11:
+                    s += ['у']
+                elif 2 <= i % 10 <= 4:
+                    s += ['ы']
+                else:
+                    s += ['']
+            await message.channel.send(f'Урок начнется в {str(dop[0]).zfill(2)}:{str(dop[1]).zfill(2)} '
+                                       f'и будет идти {dop[2]} минут{s[2]}.')
             now = [int(i) for i in str(time.asctime().split()[3]).split(':')]
             if now[0] > dop[0] or now[0] == dop[0] and now[1] >= dop[1] :
                 dop[0] += 24
             await asyncio.sleep((dop[0] * 3600 + dop[1] * 60) - (now[0] * 3600 + now[1] * 60 + now[2]))
-            lesson['At the beginning of the lesson, there were:'] = []
-            lesson['At the middle of the lesson, there were:'] = []
-            lesson['At the end of the lesson, there were:'] = []
-            await message.channel.send("The lesson has begun!")
             for guild in client.guilds:
-                for member in guild.voice_channels[0].members:
-                    lesson['At the beginning of the lesson, there were:'].append(member.name)
+                for channel in guild.voice_channels:
+                    lesson[channel.name + ':'] = {}
+                    lesson[channel.name + ':']['В начале урока присутствовали:'] = []
+                    lesson[channel.name + ':']['В середине урока присутствовали:'] = []
+                    lesson[channel.name + ':']['В конце урока присутствовали:'] = []
+            await message.channel.send("Урок начался!")
+            for guild in client.guilds:
+                for channel in guild.voice_channels:
+                    for member in channel.members:
+                        lesson[channel.name + ':']['В начале урока присутствовали:'].append(member.name)
             await asyncio.sleep(dop[2] * 30)
             for guild in client.guilds:
-                for member in guild.voice_channels[0].members:
-                    lesson['At the middle of the lesson, there were:'].append(member.name)
+                for channel in guild.voice_channels:
+                    for member in channel.members:
+                        lesson[channel.name + ':']['В середине урока присутствовали:'].append(member.name)
             await asyncio.sleep(dop[2] * 30)
             for guild in client.guilds:
-                for member in guild.voice_channels[0].members:
-                    lesson['At the end of the lesson, there were:'].append(member.name)
-            await message.channel.send("The lesson is over!")
-            title = f'The report on the lesson that started at {dop[0]}:{dop[1]}'
+                for channel in guild.voice_channels:
+                    for member in channel.members:
+                        lesson[channel.name + ':']['В конце урока присутствовали:'].append(member.name)
+            await message.channel.send("Урок окончен!")
+            title = f'Отчёт по уроку в {str(dop[0]).zfill(2)}:{str(dop[1]).zfill(2)}'
             body = []
             for key in lesson.keys():
+                body.append(' ')
                 body.append(key)
-                for i in lesson[key]:
-                    body.append(self.transliteration(i))
+                body.append(' ')
+                for i in lesson[key].keys():
+                    body.append(' ')
+                    body.append(i)
+                    for j in sorted(lesson[key][i]):
+                        body.append(j)
+                body.append(' ')
+                body.append(' ')
                 body.append(' ')
             body = '\n'.join(body)
-            letter = f'Subject: {title}\n\n{body}'
+            letter = f'{title}\n\n{body}'
             print(letter)
-            REC_EMAIL = dop[3]
-            SERVER = smtplib.SMTP('smtp.gmail.com', 587)
-            SERVER.starttls()
-            SERVER.login(SENDER_EMAIL, EMAIL_PASSWORD)
-            SERVER.sendmail(SENDER_EMAIL, REC_EMAIL, letter)
+            await client.get_user(message.author.id).send(letter)
             return
 
 
 TOKEN = str(os.environ.get('TOKEN'))
-SENDER_EMAIL = str(os.environ.get('SENDER_EMAIL'))
-EMAIL_PASSWORD = str(os.environ.get('EMAIL_PASSWORD'))
 client = YLBotClient()
 client.run(TOKEN)
 
